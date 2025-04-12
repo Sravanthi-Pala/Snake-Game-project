0303e5d4e73fb20a93f741c809bad8c4991e8f95
sravanthi <sravanthipala819@gmail.com>
2025-04-13 00:03:41 +0530

    initial commit.

---
+++ eclipse-workspace-20/atmproject/src/atmproject/AtmInterface.java
@@ -0,0 +1,9 @@
+package atmproject;
+
+public interface AtmInterface {
+public void viewBalance();
+public void withdrawAmount(double withdrawAmount);
+public void depositAmount(double depositAmount);
+public void viewMiniStatement();
+
+}
