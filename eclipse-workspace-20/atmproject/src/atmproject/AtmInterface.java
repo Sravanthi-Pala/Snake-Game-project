684a2987a5c3449472990e219de55b171ae340a2
Sravanthi-Pala <sravanthipala819@gmail.com>
2025-04-13 16:31:45 +0530

    Delete eclipse-workspace-20 directory

--- eclipse-workspace-20/atmproject/src/atmproject/AtmInterface.java
+++
@@ -1,9 +0,0 @@
-package atmproject;
-
-public interface AtmInterface {
-public void viewBalance();
-public void withdrawAmount(double withdrawAmount);
-public void depositAmount(double depositAmount);
-public void viewMiniStatement();
-
-}
